147d344c4d9facad40f72cd5c8e59b94497c77f5
salihbaki <salihbakisayer@gmail.com>
2017-05-28 21:37:51 +0300

    Add project files.

---
+++ vuslat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vuslat.Startup))]
+namespace vuslat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
